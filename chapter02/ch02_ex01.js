4ffd38a881aebec7ce716404314bcd933d1157a6
skidmarks <florakid@naver.com>
2022-09-29 17:15:52 +0900

    chapter02 first file

---
+++ chapter02/ch02_ex01.js
@@ -0,0 +1,15 @@
+var undefinedValue;
+
+console.log(undefinedValue);
+console.log("typeof undefinedValue: " + typeof(undefinedValue));
+
+/*
+    변수에 아무런 값을 할당하지 않았을 때는 어떤 데이터타입일까?
+
+    undefined  타입의 undefined 값
+
+    변수를 만들었으나 아무런 값을 부여하지 않은 경우와 정의되지 않는 변수를 나타내기 위해(console.log에서 보여지는 경우를 생각해 보라) 만들어졌다.
+
+    참고로 typeof 연산자는 데이터 타입을 문자열로 리턴한다.
+
+*/
