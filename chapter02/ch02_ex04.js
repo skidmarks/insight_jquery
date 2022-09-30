e77e17659782b52a6d11b015cd9daecad0df2924
skidmarks <florakid@naver.com>
2022-09-30 10:07:56 +0900

    chapter02 ex07

---
+++ chapter02/ch02_ex04.js
@@ -0,0 +1,15 @@
+var a1 = [100, 200, 300];
+
+var a2 = new Array();
+a2[0] = 100;
+a2[1] = 200;
+a2[2] = 300;
+
+console.log(a1);
+console.log(a2);
+
+console.dir(a1);
+console.dir(a2);
+
+console.table(a1);
+console.table(a2);
